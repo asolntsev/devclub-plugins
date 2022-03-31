5c3449aa9d02d7ed230e3094fbb1137c0a64300d
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-03-31 09:08:49 +0300

    how to mock final class

--- src/main/java/eu/devclub/plugins/UserRepository.java
+++ src/main/java/eu/devclub/plugins/UserRepository.java
@@ -2,7 +2,7 @@ package eu.devclub.plugins;
 
 import java.util.Optional;
 
-public class UserRepository {
+public final class UserRepository {
   public Optional<Long> findUser(String username) {
     return Optional.of(1L);
   }
