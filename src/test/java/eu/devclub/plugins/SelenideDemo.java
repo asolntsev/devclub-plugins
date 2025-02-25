fc3de045392f3fc21fb5d99f46052bb4bbdffb30
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-02-25 20:51:37 +0200

    fix existing UI test and run it on CI

--- src/test/java/eu/devclub/plugins/SelenideDemo.java
+++ src/test/java/eu/devclub/plugins/SelenideDemo.java
@@ -1,19 +1,32 @@
 package eu.devclub.plugins;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.Test;
-
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.CollectionCondition.sizeLessThan;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
+import org.junit.jupiter.api.Test;
 
 public class SelenideDemo {
   @Test
   void searchJavaPlugins() {
-    open("https://duckduckgo.com");
+    open("https://selenide.org");
+    $(".main-menu-pages").find(byText("Users")).click();
 
-    $("[name=q]").val("java plugins");
-    $("#search_button_homepage").click();
+    $$("#user-tags .tag").shouldHave(sizeGreaterThan(8));
+    $$("#selenide-users .user:not(.hidden)").shouldHave(size(1));
 
-    $$("#links .result").shouldHave(sizeGreaterThan(5));
+    $("#user-tags").find(byTagAndText("a", "all")).click();
+    $$("#selenide-users .user:not(.hidden)").shouldHave(sizeGreaterThan(80));
+
+    $("#user-tags").find(byTagAndText("a", "ukraine")).click();
+    $$("#selenide-users .user:not(.hidden)").shouldHave(sizeLessThan(80));
+
+    $("#user-tags").find(byTagAndText("a", "healthcare")).click();
+    $$("#selenide-users .user:not(.hidden)").shouldHave(sizeLessThan(20));
   }
 }
